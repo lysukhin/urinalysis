ff9bdbca64906e52b05ad9908b2c5563b5f51283
byakimov <bp.yakimov@gmail.com>
2017-09-06 16:01:13 +0300

    Добавлено описание функций, изменена метрика. Текущая точность на тестовом датасете 86% (точное попадание в класс)  и 100% (ошибка в 1 класс в любую сторону)

--- scripts/neighbours.py
+++ scripts/neighbours.py
@@ -4,25 +4,57 @@ import cv2
 
 
 
-def get_manhattan_distance(img1,img2):
+def get_weighted_manhattan_distance(img1,img2):
+    """
+    Current state:
+    calculate mean values of channels of images img1, img2:
+    and then calculate manhattan distance with weights over them.
+
+    weights = [1,5,4] were chosen using grid search 
+    (! both images should be in LAB color space !)
     
+    Takes as input:
+    
+    - img1, img2 - numpy 3d arrays 
+    
+    Returns:
+    
+    - distance - float, distance between two input images
+    """
+    weights = [2.,5.,3.]
     point1 = np.mean(img1,axis=(0,1))
     point2 = np.mean(img2,axis=(0,1))
     d = 0.
     
     for i in xrange(point1.shape[0]):
-        d += np.abs(point1[i] - point2[i])
+        d += weights[i]*np.abs(point1[i] - point2[i])
         
     return d
     
 def preprocessing(img):
+    
+    """
+    Current state: 
+    - do blur with kernel size = (5,5)
+    - convert from RGB to LAB space
+    
+    Takes as input:
+    
+    - img - numpy 3d array (color image)
+    
+    Returns:
+    
+    - img - processed image, numpy 3d array 
+    
+    
+    """
     img = cv2.blur(img,(5,5))
     return cv2.cvtColor(img,cv2.COLOR_RGB2LAB)
     
 
 def one_nearest_neighbor(train_images,test_img,verbose=False,
                          preprocessing=preprocessing,
-                         get_distance=get_manhattan_distance,
+                         get_distance=get_weighted_manhattan_distance,
                          return_probabilities=False):
     """
     Takes as input: 
@@ -48,6 +80,11 @@ def one_nearest_neighbor(train_images,test_img,verbose=False,
     
     
     ---------------------
+        
+    Accuracy on current dataset is 86% to recognize correct class
+    and 100% to make mistake +/- 1 class. 
+    
+    !This is need to be checked on other datasets!
     
     probabilities are counted like softmax(distance): 
     
